e31155f0947f3fe4e50e9d645ca5c9082993ca8d
Umay Soyluer <umichan@Umays-MacBook-Pro.local>
2023-12-06 16:40:38 +0100

    first commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, columnIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][columnIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((activePlayerSymbol, columnIndex) => (
+              <li key={columnIndex}>
+                <button
+                  onClick={() => handleSelectSquare(rowIndex, columnIndex)}
+                >
+                  {activePlayerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default GameBoard;
---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEdit() {
+    setIsEditing((prev) => !prev);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{isEditing ? "SAVE" : "EDIT"}</button>
+    </li>
+  );
+}
+
+export default Player;
